65a4b884966b4cf25131acee7ff4e9989096e071
Pavan Kumar reddy T <pavankreddy.t@gmail.com>
2024-02-12 21:19:35 +0530

    Changes

--- UIComponents/app/src/main/java/com/nareshittechnologies/uicomponents/MainActivity.kt
+++ UIComponents/app/src/main/java/com/nareshittechnologies/uicomponents/MainActivity.kt
@@ -3,11 +3,19 @@ package com.nareshittechnologies.uicomponents
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.CheckBox
 import android.widget.RadioGroup
+import android.widget.Spinner
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatSpinner
+import androidx.appcompat.widget.SwitchCompat
 import com.google.android.material.textfield.TextInputLayout
+import java.util.Objects
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,6 +28,45 @@ class MainActivity : AppCompatActivity() {
         val pa:TextInputLayout = findViewById(R.id.person_age)
         val result:TextView = findViewById(R.id.result)
         val gender_group:RadioGroup = findViewById(R.id.gender_group)
+        val country:AppCompatSpinner = findViewById(R.id.spinner)
+        val index:Int = country.selectedItemPosition
+        val c =  country.adapter.getItem(index) as String
+
+        val sc:SwitchCompat = findViewById(R.id.switch_tog)
+        var rotationalShift = false
+        if(sc.isChecked){
+            rotationalShift = true
+        }
+        /*var c:String = ""
+        country.setOnItemSelectedListener(object:AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                c = parent?.selectedItem as String
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        })*/
+        val eng:CheckBox = findViewById(R.id.eng)
+        val hin:CheckBox = findViewById(R.id.hin)
+        val tam:CheckBox = findViewById(R.id.tam)
+
+        var languages:String = ""
+        if(eng.isChecked){
+            languages = languages + "English\n"
+        }
+        if(hin.isChecked){
+            languages = languages+"Hindi\n"
+        }
+        if(tam.isChecked){
+            languages = languages + "Tamil\n"
+        }
         var g:String = "Male"
         gender_group.setOnCheckedChangeListener { group, checkedId ->
             if(checkedId == R.id.male){
@@ -28,11 +75,12 @@ class MainActivity : AppCompatActivity() {
                 g = "Female"
             }
         }
-
         val name = pn.editText?.text.toString()
         val age = pa.editText?.text.toString().toInt()
 
-        result.text = "$name  $age $g"
+
+
+        result.text = "$c $rotationalShift"
 
     }
 }
